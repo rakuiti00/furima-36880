5ae68c3449e69cedb46a37f67321a2a95443dab0
rakuiti00 <rakuiti77@gmail.com>
2021-09-25 11:04:35 +0900

    DBへの登録処理を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,37 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new]
+
   # トップページの表示
   def index
   end
 
   # 商品出品ページの表示
   def new
+    @item = Item.new
+  end
+
+  #商品情報の保存
+  def create
+    @item = Item.new(item_params)
+    binding.pry
+
+    # 保存判定
+    if @item.save
+      redirect_to root_path      
+    else
+      render :new      
+    end
     
   end
+
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:description,:category_id,:item_status_id,:shipping_charges_id,:shipping_area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
+  end
+
+
 
 end
