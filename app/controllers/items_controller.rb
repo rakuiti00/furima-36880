638afc5a90602414240ffa23c14b15e9dadee7f4
rakuiti00 <rakuiti77@gmail.com>
2021-09-28 15:32:46 +0900

    不正な遷移の防止処理追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC ")
@@ -44,6 +45,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:name,:description,:category_id,:item_status_id,:shipping_charges_id,:shipping_area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
 
+    if !(user_signed_in? && current_user.id == @item.user.id) 
+      redirect_to action: :index
+      
+    end
 
+  end
 end
