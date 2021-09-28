8f1b60cf2ee811b8b1dc365d46013b05b8b39865
rakuiti00 <rakuiti77@gmail.com>
2021-09-28 15:53:21 +0900

    重複した記述のまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show]
+
 
   def index
     @items = Item.order("created_at DESC ")
@@ -22,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
@@ -44,11 +45,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:name,:description,:category_id,:item_status_id,:shipping_charges_id,:shipping_area_id,:days_to_ship_id,:price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])    
+  end
+
   def move_to_index
     @item = Item.find(params[:id])
 
     if !(user_signed_in? && current_user.id == @item.user.id) 
       redirect_to action: :index
+      
     end
 
   end
