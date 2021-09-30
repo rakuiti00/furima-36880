3ddd9aa5b72de7e8988070cd1762743d0706e6e9
rakuiti00 <rakuiti77@gmail.com>
2021-09-30 12:34:51 +0900

    トークン化に成功まで

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,32 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_ba96801c063aae04dc1be714");
   const submit = document.getElementById("button");
   submit.addEventListener("click",(e) => {
     e.preventDefault();
-    console.log("フォーム送信ボタン押下時にイベント発火を確認")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_destination[number]"),
+      cvc: formData.get("purchase_destination[cvc]"),
+      exp_month: formData.get("purchase_destination[exp_month]"),
+      exp_year: `20${formData.get("purchase_destination[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status,response) => {
+      if(status == 200) {
+        const token = response.id;
+        
+      }
+
+
+    });
+
+
+
+
+
   });
 
 };
