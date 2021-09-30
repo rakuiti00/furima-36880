f6280b27d55e38a3ed3d00a4aaf19d1251d2bce9
rakuiti00 <rakuiti77@gmail.com>
2021-09-30 11:54:27 +0900

    正規表現修正

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -6,10 +6,10 @@ class PurchaseDestination
   
     with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-      validates :shipping_area_id
+      validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
       validates :city
       validates :address
-      validates :phone_number #, format: {with: /\A\d{10}\z/}
+      validates :phone_number, format: {with: /\A\d{11}\z|\A\d{10}\z/}
       validates :user_id
       validates :item_id
     end
