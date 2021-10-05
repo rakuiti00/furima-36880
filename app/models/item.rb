1ad6ee7e766c3204192c1eec55854f1f836df816
rakuiti00 <rakuiti77@gmail.com>
2021-10-05 17:56:21 +0900

    エラーメッセージ日本語化の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,11 +16,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :price, presence: true
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charges_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :item_status_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :shipping_charges_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "を選択してください"}
 
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                   format: { with: /\A[0-9]+\z/ }
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -6,7 +6,7 @@ class PurchaseDestination
   
     with_options presence: true do
       validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-      validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+      validates :shipping_area_id, numericality: { other_than: 1 , message: "を選択してください"}
       validates :city
       validates :address
       validates :phone_number, format: {with: /\A\d{11}\z|\A\d{10}\z/}
